a76ecd0a7e4c42c3ac809e08f8335ecadcd9644d
wavescats <wavescats@gmail.com>
2022-12-26 11:25:15 +0900

    [FEAT] ball bounce

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -7,28 +7,50 @@ import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { angleToRadians } from "../../utils/angle";
 import * as THREE from "three";
+import gsap from "gsap";
 
 export default function Three() {
-  const OrbitControlsRef = useRef(null);
+  const orbitControlsRef = useRef(null);
+  const ballRef = useRef(null);
 
   useFrame(e => {
-    if (OrbitControlsRef.current) {
+    if (orbitControlsRef.current) {
       const { x, y } = e.mouse;
       // x축 제어
-      OrbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(45));
+      orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(45));
 
       // y축 제어
-      OrbitControlsRef.current.setPolarAngle((y + 1) * angleToRadians(90 - 30));
+      orbitControlsRef.current.setPolarAngle((y + 1) * angleToRadians(90 - 30));
 
       // 업데이트
-      OrbitControlsRef.current.update();
+      orbitControlsRef.current.update();
     }
   });
+
   useEffect(() => {
-    if (OrbitControlsRef.current) {
-      console.log(OrbitControlsRef.current);
+    if (ballRef.current) {
+      const ballmove = gsap.timeline({ paused: true });
+
+      ballmove.to(ballRef.current.position, {
+        x: 1,
+        duration: 2,
+        ease: "power2.out",
+      });
+
+      ballmove.to(
+        ballRef.current.position,
+        {
+          y: 0.5,
+          duration: 0.75,
+          ease: "bounce.out",
+        },
+        // ">+=3"  위에 out 애니메이션이 다 실행된 후에 이어받아서 실행
+        "<"
+      );
+      ballmove.play();
     }
-  }, [OrbitControlsRef.current]);
+  }, [ballRef.current]);
+
   return (
     <>
       {/* 카메라 */}
@@ -36,13 +58,13 @@ export default function Three() {
 
       {/* 카메라 컨트롤 */}
       <OrbitControls
-        ref={OrbitControlsRef}
+        ref={orbitControlsRef}
         minPolarAngle={angleToRadians(60)}
         maxPolarAngle={angleToRadians(80)}
       />
 
       {/* 공 */}
-      <mesh position={[0, 0.5, 0]} castShadow>
+      <mesh position={[-2, 1.5, 0]} castShadow ref={ballRef}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#ffffff" metalness={0.6} roughness={0.5} />
       </mesh>
